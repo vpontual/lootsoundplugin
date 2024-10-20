92a9fa5563050e35a116f59db29c1a72a8071f0e
veepee <66334129+vpontual@users.noreply.github.com>
2024-10-20 09:28:34 -0700

    added lua code to play sound

---
+++ LootSoundPlugin.lua
@@ -0,0 +1,18 @@
+local addonName = "LootSoundPlugin"
+local addonVersion = "1.0"
+
+function LootSoundPlugin:OnLoad()
+    self.name = addonName
+    self.version = addonVersion
+    self.soundFile = ./sounds/treasure.mp3
+
+    -- Registering the event handler for loot pickup
+    self:RegisterEvent("LOOT_PICKED_UP")
+end
+
+function LootSoundPlugin:OnEvent(event)
+    if event == "LOOT_PICKED_UP" then
+        -- Playing the sound using PlaySound()
+        PlaySound(self.soundFile)
+    end
+end
