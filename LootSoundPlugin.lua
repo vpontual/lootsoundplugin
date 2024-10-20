d418916a25290d01b933ea51658625d6e37a3303
veepee <66334129+vpontual@users.noreply.github.com>
2024-10-20 10:51:16 -0700

    updated lua and toc to follow updated WOW addon guidelines

--- LootSoundPlugin.lua
+++ LootSoundPlugin.lua
@@ -1,24 +1,17 @@
-local addonName = "LootSoundPlugin"
-local addonVersion = "1.0"
+local addonName, addon = ...
+addon.version = "1.0"
 
 local playerGUID = UnitGUID("player")
 local SOUND_PATH = "Interface\\AddOns\\LootSoundPlugin\\sounds\\treasure.ogg"
 local SOUND_CHANNEL = "Master"
 
-function LootSoundPlugin:OnLoad()
-  self.name = addonName
-  self.version = addonVersion
+local frame = CreateFrame("Frame")
 
-  -- Registering the event handler for loot pickup
-  self:RegisterEvent("LOOT_PICKED_UP")
-end
+frame:RegisterEvent("LOOT_OPENED")
 
-function LootSoundPlugin:OnEvent(event, ...)
-  if event == "LOOT_PICKED_UP" then
-    local _, _, _, sourceGUID = select(4, ...)
-    if sourceGUID == playerGUID then
-      -- Playing the sound using PlaySoundFile()
-      PlaySoundFile(SOUND_PATH, SOUND_CHANNEL)
+frame:SetScript("OnEvent", function(self, event, ...)
+    if event == "LOOT_OPENED" then
+        -- Playing the sound using PlaySoundFile()
+        PlaySoundFile(SOUND_PATH, SOUND_CHANNEL)
     end
-  end
-end
+end)
